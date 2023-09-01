173ce0f57e8b0d51bfdc328e3089bed29128c1bc
Badsha Hossain <badshab454@gmail.com>
2023-09-01 23:52:30 +0600

    react icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {AiOutlinePlus} from 'react-icons/ai';
 
 const App = () => {
   return (
-    <div className="text-red-500">App</div>
+    <div className="h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]">
+      <h1 className="text-4xl font-bold">Todo App</h1>
+      <form action="">
+        <input type="text" placeholder="Add Todo" />
+        <button><AiOutlinePlus size={30}/></button>
+      </form>
+    </div>
   )
 }
 
